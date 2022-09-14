d6f4da7971626bef2fa97c3cd0c1441c97dd7659
Erandi del Angel <a01379646@tec.mx>
2022-09-13 19:02:33 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import React, { useState } from "react";
+import React, { Component} from 'react';
+import {
+  StyleSheet,
+  Text, 
+  View, 
+  Dimensions,
+  TouchableOpacity} from 'react-native';
 
-import ListingEditingScreen from "./app/screens/ListingEditingScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 
-export default function App() {
-	return <WelcomeScreen />;
-	// return <WelcomeScreen />;
-	// return <ListingEditingScreen />;
-}
+//Navigation import
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+//Screen One
+const ScreenOne = props => {
+
+  //onPress To Navigate
+  const onPress = () => {
+    props.navigation.navigate('ScreenTwo');
+  };
+
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <TouchableOpacity onPress={onPress}>
+        <Text>Hello From Screen One</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+//Screen Two
+const ScreenTwo = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Screen Two</Text>
+    </View>
+  );
+};
+
+const App = () => {
+  //const
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ScreenOne" component={ScreenOne} />
+        <Stack.Screen name="ScreenTwo" component={ScreenTwo} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
