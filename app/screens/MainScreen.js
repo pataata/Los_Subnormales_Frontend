8f93f982a652c3bf1624a769f21e6d166ac3886c
Rbn-Sanchez <A01378379@itesm.mx>
2022-09-15 00:38:39 -0500

    Interfaz funcionando

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -11,14 +11,72 @@ import Slider from '@react-native-community/slider';
 import Checkbox from "expo-checkbox";
 import ModalDropdown from "react-native-modal-dropdown";
 
-const validationSchema = Yup.object().shape({
-	email: Yup.string().required().email().label("Email"),
-	password: Yup.string().required().min(4).label("Password"),
-});
+
 
 function MainScreen({ navigation }) {
-	const [isChecked, setChecked] = useState(false);
+	const defaultValues = {'Room Service': '100', 'CryoSleep': true, 'Spa':150,'VR Deck': '50', 'VIP': false, 'Food Court': '150', 'Cabin': 'D',
+							'Age':30,'Home Planet':'Earth','Side':'Port','Shopping Mall':'40','Destination':'55 Cancrie','Group ID':'08'}
 
+	const [RoomService, setRoomService] = useState(defaultValues['Room Service'])
+	const [CryoSleep, setCryoSleep] = useState(defaultValues['CryoSleep'])
+	const [Spa, setSpa] = useState(defaultValues['Spa'])
+	const [VRDeck, setVRDeck] = useState(defaultValues['VR Deck'])
+	const [VIP, setVIP] = useState(defaultValues['VIP'])
+	const [FoodCourt, setFoodCourt] = useState(defaultValues['Food Court'])
+	const [Cabin, setCabin] = useState(defaultValues['Cabin'])
+	const [Age, setAge] = useState(defaultValues['Age'])
+	const [HomePlanet, setHomePlanet] = useState(defaultValues['Home Planet'])
+	const [Side, setSide] = useState(defaultValues['Side'])
+	const [ShoppingMall, setShoppingMall] = useState(defaultValues['Shopping Mall'])
+	const [Destination, setDestination] = useState(defaultValues['Destination'])
+	const [GroupId, setGroupId] = useState(defaultValues['Group ID'])
+
+	function changeValues(title,val){
+		switch(title){
+			case 'Room Service':
+				setRoomService(val) 
+				break;
+			case 'CryoSleep':
+				setCryoSleep(val)
+				break;
+			case 'Spa':
+				setSpa(val) 
+				break;
+			case 'VR Deck':
+				setVRDeck(val)
+			case 'VIP':
+				setVIP(val) 
+				break;
+			case 'Food Court':
+				setFoodCourt(val) 
+				break;
+			case 'Cabin':
+				setCabin(val) 
+				break;
+			case 'Age':
+				setAge(val) 
+				break;
+			case 'Home Planet':
+				setHomePlanet(val) 
+				break;
+			case 'Side':
+				setSide(val) 
+				break;
+			case 'Destination':
+				setDestination(val) 
+				break;
+			case 'Group ID':
+				setGroupId(val) 
+				break;
+		}
+	}
+
+	function setText(title){
+		if(title==='Spa')
+			return Spa
+		else
+			return Age
+	}
 
 	return (
 		<Screen style={styles.container}>
@@ -30,40 +88,58 @@ function MainScreen({ navigation }) {
 			<Image style={styles.logo} source={require("../assets/titanic-espacial.png")} />
 			<FlatList
 				data={[
-				{name: 'textinput', title: 'Room Service', defvalue: 1600},
+				{name: 'textinput', title: 'Room Service'},
 				{name: 'checkbox', title: 'CryoSleep'},
-				{name: 'slider', title: 'Spa'},
-				{name: 'textinput', title: 'VR Deck', defvalue: 1600},
+				{name: 'slider', title: 'Spa', 'min': 0, 'max': 200},
+				{name: 'textinput', title: 'VR Deck'},
 				{name: 'switch', title: 'VIP'},
-				{name: 'textinput', title: 'FoodCourt', defvalue: 1600},
-				{name: 'dropdown', title: 'Cabin', options: ['A','C','E','D','B','T'], defVal: 'C'},
-				{name: 'slider', title: 'Age'},
-				{name: 'dropdown', title: 'Home Planet', options: ['Europa','Earth','Mars'], defVal: 'Mars'},
-				{name: 'textinput', title: 'Total Bill', defvalue: 1600},
-				{name: 'dropdown', title: 'Side', options: ['Port','Starboard'], defVal: 'Port'},
-				{name: 'dropdown', title: 'Destination', options: ['55 Cancrie','PSOJ318.5-22','TRAPPIST-1e'], defVal: '55 Cancrie'},
-				{name: 'dropdown', title: 'Group ID', options: ['01','08'], defVal: '01'},
+				{name: 'textinput', title: 'Food Court'},
+				{name: 'dropdown', title: 'Cabin', options: ['A','C','E','D','B','T']},
+				{name: 'slider', title: 'Age', 'min': 0, 'max': 100},
+				{name: 'dropdown', title: 'Home Planet', options: ['Europa','Earth','Mars']},
+				{name: 'dropdown', title: 'Side', options: ['Port','Starboard']},
+				{name: 'textinput', title: 'Shopping Mall'},
+				{name: 'dropdown', title: 'Destination', options: ['55 Cancrie','PSOJ318.5-22','TRAPPIST-1e']},
+				{name: 'dropdown', title: 'Group ID', options: ['01','08']},
 				]}
         		renderItem={({item}) => {
 					if(item.name === "textinput"){
 						return(
 						<View style={styles.viewContainer}>
 							<Text style={styles.itemcenter}>{item.title}</Text>
-							<TextInput style={styles.textinput}>{item.defvalue}</TextInput>
+							<TextInput 
+								style={styles.textinput}
+								onChangeText={text => changeValues(item.title,text)}
+								defaultValue={defaultValues[item.title]}
+								></TextInput>
 						</View>
 						)
 					}else if(item.name === 'switch'){
 						return(
 						<View style={styles.viewContainer}>
 							<Text style={styles.item}>{item.title}</Text>
-							<Switch/>
+							<Switch
+								style={styles.switch}
+								trackColor={{ false: "#767577", true: "#81b0ff" }}
+								thumbColor={VIP ? "#f5dd4b" : "#f4f3f4"}
+								ios_backgroundColor="#3e3e3e"
+								onValueChange={val => changeValues(item.title, val)}
+								value={VIP}
+								/>
 						</View>
 						)
 					}else if(item.name === 'slider'){
 						return(
 							<View style={styles.viewContainer}>
 								<Text style={styles.item}>{item.title}</Text>
-								<Slider/>
+								<Slider
+									value={defaultValues[item.title]}
+									minimumValue={item.min}
+									maximumValue={item.max}
+									step={1}
+									onValueChange={sliderValue => changeValues(item.title, sliderValue)}
+								/>
+								<Text style={styles.textSlider}>{setText(item.title)}</Text>
 							</View>
 						)
 					}else if(item.name === 'checkbox'){
@@ -72,9 +148,9 @@ function MainScreen({ navigation }) {
 								<Text style={styles.item}>{item.title}</Text>
 								<Checkbox 
 									style={styles.checkbox}
-									value={isChecked}
-          							onValueChange={setChecked}
-									color={isChecked ? '#4630EB' : undefined}
+									value={CryoSleep}
+          							onValueChange={val => changeValues(item.title, val)}
+									color={CryoSleep ? '#4630EB' : undefined}
 								/>
 							</View>
 						)
@@ -86,9 +162,10 @@ function MainScreen({ navigation }) {
 									style = {styles.dropdown}
 									options = {item.options}
 									textStyle = {styles.dropdowntext}
-									defaultValue = {item.defVal}
+									defaultValue = {defaultValues[item.title]}
 									dropdownStyle = {styles.dropdownmenu}
 									dropdownTextStyle = {styles.dropdowntextstyle}
+									onSelect={val => changeValues(item.title, item.options[val])}
 								/>
 							</View>
 						)
@@ -106,12 +183,37 @@ function MainScreen({ navigation }) {
 			<AppButton 
 				style={styles.button}
 				title="Calcular"
-				onPress={() => navigation.navigate('Results')}/>
+				onPress={() => {
+					//navigation.navigate('Results')
+				
+					console.log(
+						{
+							'RoomService': RoomService,
+							'CryoSleep': CryoSleep,
+							'Spa': Spa,
+							'VRDeck': VRDeck,
+							'VIP': VIP,
+							'FoodCourt': FoodCourt,
+							'Cabin': Cabin,
+							'HomePlanet': HomePlanet,
+							'Age': Age,
+							'Side': Side,
+							'ShoppingMall': ShoppingMall,
+							'Destination': Destination,
+							'GroupID': GroupId
+						}
+					)
+				}}/>
 		</Screen>
 	);
 }
 
 const styles = StyleSheet.create({
+	textSlider:{
+		color: colors.text,
+		alignSelf: 'center'
+	},
+
 	container: {
 		padding: 10,
 		backgroundColor: colors.background
@@ -151,6 +253,10 @@ const styles = StyleSheet.create({
 
 	checkbox: {
 		margin: 8,
+	  },
+
+	switch: {
+		marginTop: 8,
 	  },
 	
 	textinput: {
