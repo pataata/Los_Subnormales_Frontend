438baaae8df218265fbef063cd1527746105b227
Rbn-Sanchez <A01378379@itesm.mx>
2022-09-14 20:23:09 -0500

    Se agrego pantalla de resultados

--- app/screens/Results.js
+++ app/screens/Results.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import {Button, Text} from "react-native";
+import {Button, Text, StyleSheet} from "react-native";
+import AppButton from '../components/AppButton';
+import Screen from '../components/Screen';
+import colors from '../config/colors';
 
 
 function Results({navigation}) {
     return (
-        <Button title="< Regresar" onPress={() => navigation.navigate('MainScreen')}/>
+        <Screen style={styles.background}>
+            <Button title="< Regresar" onPress={() => navigation.navigate('MainScreen')}/>
+            <AppButton title="boton que no hace nada"></AppButton>
+        </Screen>
     );
 }
+
+const styles = StyleSheet.create({
+    background:{
+        padding: 10,
+		backgroundColor: colors.background
+    }
+})
 
 export default Results;
