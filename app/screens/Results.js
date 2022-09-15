29c9beb93c008424e7f887c410491a7298e253b0
Erandi del Angel <a01379646@tec.mx>
2022-09-15 15:15:14 -0500

    f

--- app/screens/Results.js
+++ app/screens/Results.js
@@ -7,16 +7,18 @@ import colors from '../config/colors';
 
 function Results({navigation, route}) {
     var mensaje = 'Lo sentimos, parece que viajarás a otra dimensión alterna.'
+    var colorcirculo = styles.circulorojo
 
     if(route.params.resultado === '0'){
         mensaje = 'Felicidades, viajas seguro'
+        colorcirculo = styles.circuloverde
     }
 
     return (
         <Screen style={styles.background}>
             <Button title="< Regresar" onPress={() => navigation.navigate('MainScreen')}/>
             <View style={styles.space} />
-            <View style = {styles.circle}>
+            <View style = {colorcirculo}>
               <Text style={styles.buttontext}> {mensaje} </Text>
             </View>
             <View style={styles.space} />
@@ -32,11 +34,20 @@ const styles = StyleSheet.create({
         alignItems: 'center', 
         flex: 1
     },
-    circle: {
+    circuloverde: {
         borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height),
         width: 350,
         height: 350,
-        backgroundColor: colors.text,
+        backgroundColor: colors.background3,
+        justifyContent: "center",
+        alignSelf: 'center',
+        padding : 15
+    },
+    circulorojo: {
+        borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height),
+        width: 350,
+        height: 350,
+        backgroundColor: colors.danger,
         justifyContent: "center",
         alignSelf: 'center',
         padding : 15
